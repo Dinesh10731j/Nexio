e406eb16515d82340ce1cd60835a1c1648f783b9
Dinesh10731j <dinesh1122@gmail.com>
2024-10-22 22:05:17 +0545

    minor design change in signup page

--- src/app/(landingpage)/login/page.tsx
+++ src/app/(landingpage)/login/page.tsx
@@ -1,24 +1,28 @@
-"use client"
-import React from 'react';
-import Header from '@/components/header';
-import Footer from '@/components/footer';
-import { Input } from '@/components/ui/input';
-import { Button } from '@/components/ui/button';
-import Link from 'next/link';
-import Image from 'next/image';
+"use client";
+import React from "react";
+import Header from "@/components/header";
+import Footer from "@/components/footer";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+import Image from "next/image";
 import dummyImage from "../../../assets/Image.png";
-import {useForm,SubmitHandler} from "react-hook-form";
-import { loginType } from '@/types/Types';
+import { useForm, SubmitHandler } from "react-hook-form";
+import { loginType } from "@/types/Types";
 
 const Login = () => {
- const {handleSubmit,register,formState:{errors},reset} = useForm<loginType>();
+  const {
+    handleSubmit,
+    register,
+    formState: { errors },
+    reset,
+  } = useForm<loginType>();
 
- const onLogin:SubmitHandler<loginType> = (data)=>{
-  console.log(data);
+  const onLogin: SubmitHandler<loginType> = (data) => {
+    console.log(data);
 
-  reset();
-
- }
+    reset();
+  };
 
   return (
     <>
@@ -35,41 +39,58 @@ const Login = () => {
           </div>
           {/* Login Form Section */}
           <div className="w-full p-8 md:w-1/2">
-            <h2 className="text-3xl font-bold text-center text-gray-800 mb-6">Login to Your Account</h2>
+            <h2 className="text-3xl font-bold text-center text-gray-800 mb-6">
+              Login to Your Account
+            </h2>
             <form className="space-y-4" onSubmit={handleSubmit(onLogin)}>
               <div>
-                <label className="block text-sm font-medium text-gray-700" htmlFor="email">
+                <label
+                  className="block text-sm font-medium text-gray-700"
+                  htmlFor="email"
+                >
                   Email
                 </label>
                 <Input
-                  
                   type="email"
-                  {...register('email',{required:'E-mail is required',validate:{
-                    isValidEmail:(value)=>{
-                      return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(value) || 'Please enter a valid email address';
-                    }
-                  }})}
-                 
+                  {...register("email", {
+                    required: "E-mail is required",
+                    validate: {
+                      isValidEmail: (value) => {
+                        return (
+                          /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(value) ||
+                          "Please enter a valid email address"
+                        );
+                      },
+                    },
+                  })}
                   className="mt-1 block w-full p-2 border border-gray-300 rounded-md shadow-sm focus:ring-blue-500 focus:border-blue-500"
                   placeholder="example@mail.com"
                 />
-
-                {errors.email && <p className='text-sm text-red-700'>{errors.email?.message}</p>}
               </div>
+              {errors.email && (
+                <p className="text-sm text-red-700">{errors.email?.message}</p>
+              )}
               <div>
-                <label className="block text-sm font-medium text-gray-700" htmlFor="password">
+                <label
+                  className="block text-sm font-medium text-gray-700"
+                  htmlFor="password"
+                >
                   Password
                 </label>
                 <Input
-                {...register('password',{required:'Password is required'})}
-                 
+                  {...register("password", {
+                    required: "Password is required",
+                  })}
                   type="password"
-                
                   className="mt-1 block w-full p-2 border border-gray-300 rounded-md shadow-sm focus:ring-blue-500 focus:border-blue-500"
                   placeholder="••••••••"
                 />
               </div>
-              {errors.password && <p className='text-sm text-red-700'>{errors.password?.message}</p>}
+              {errors.password && (
+                <p className="text-sm text-red-700">
+                  {errors.password?.message}
+                </p>
+              )}
               <div>
                 <Button
                   type="submit"
@@ -80,7 +101,7 @@ const Login = () => {
               </div>
             </form>
             <p className="text-center text-gray-600 mt-4">
-              Don&apos;t have an account?{' '}
+              Don&apos;t have an account?{" "}
               <Link href="/signup" className="text-blue-600 hover:underline">
                 Signup
               </Link>
