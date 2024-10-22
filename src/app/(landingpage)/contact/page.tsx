caf10435afbfb3dedc196e30e2ec1c8521a9ebae
Dinesh10731j <dinesh1122@gmail.com>
2024-10-22 19:00:53 +0545

    contact form is validated by using react -hook-from

--- src/app/(landingpage)/contact/page.tsx
+++ src/app/(landingpage)/contact/page.tsx
@@ -1,14 +1,28 @@
-import React from 'react';
-import Header from '@/components/header';
-import Footer from '@/components/footer';
-import Image from 'next/image';
+"use client";
+import React from "react";
+import Header from "@/components/header";
+import Footer from "@/components/footer";
+import Image from "next/image";
 import dummyImage from "../../../assets/Image.png";
-import { Input } from '@/components/ui/input';
-import { Textarea } from '@/components/ui/textarea';
-import { Button } from '@/components/ui/button';
-
+import { Input } from "@/components/ui/input";
+import { Textarea } from "@/components/ui/textarea";
+import { Button } from "@/components/ui/button";
+import { useForm, SubmitHandler } from "react-hook-form";
+import { contactType } from "@/types/Types";
 
 const Contact = () => {
+  const {
+    handleSubmit,
+    register,
+    reset,
+    formState: { errors },
+  } = useForm<contactType>();
+
+  const onContactSubmit: SubmitHandler<contactType> = (data) => {
+    console.log(data);
+
+    reset();
+  };
   return (
     <>
       <Header />
@@ -18,47 +32,92 @@ const Contact = () => {
             Contact Us
           </h2>
           <p className="text-center text-gray-600 mb-8">
-            Feel free to reach out to us via the form below or through our contact information.
+            Feel free to reach out to us via the form below or through our
+            contact information.
           </p>
 
           <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
             {/* Contact Form */}
-            <form className="space-y-6 md:col-span-2">
+            <form
+              className="space-y-6 md:col-span-2"
+              onSubmit={handleSubmit(onContactSubmit)}
+            >
               <div>
-                <label className="block text-sm font-medium text-gray-700" htmlFor="name">
+                <label
+                  className="block text-sm font-medium text-gray-700"
+                  htmlFor="name"
+                >
                   Name
                 </label>
                 <Input
-                
-                  type='text'
-                placeholder='Enter name'
-                  required
+                  {...register("name", {
+                    required: "Name is required",
+                    validate: {
+                      minLength: (value) =>
+                        value.length > 3 ||
+                        "Name must be more than 3 characters",
+                    },
+                  })}
+                  type="text"
+                  placeholder="Enter name"
                   className="mt-1 block w-full p-2 border border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500"
                 />
               </div>
 
+              {errors.name && (
+                <p className="text-sm text-red-700">{errors.name?.message}</p>
+              )}
+
               <div>
-                <label className="block text-sm font-medium text-gray-700" htmlFor="email">
+                <label
+                  className="block text-sm font-medium text-gray-700"
+                  htmlFor="email"
+                >
                   Email
                 </label>
                 <Input
-                type='email'
-                 placeholder='Enter e-mail'
-                  required
+                  type="email"
+                  placeholder="Enter e-mail"
+                  {...register("email", {
+                    required: "E-mail is required",
+                    validate: {
+                      isValidEmail: (value) =>
+                        /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(value) ||
+                        "Please enter a valid email address",
+                    },
+                  })}
                   className="mt-1 block w-full p-2 border border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500"
                 />
               </div>
+              {errors.email && (
+                <p className="text-sm text-red-700">{errors.email?.message}</p>
+              )}
 
               <div>
-                <label className="block text-sm font-medium text-gray-700" htmlFor="message">
+                <label
+                  className="block text-sm font-medium text-gray-700"
+                  htmlFor="message"
+                >
                   Message
                 </label>
                 <Textarea
-                  placeholder='Your message'
-                  required
+                  placeholder="Your message"
+                  {...register("message", {
+                    required: "Message is required",
+                    validate: {
+                      minLength: (value) =>
+                        value.length > 10 ||
+                        "Message must be more than 10 characters",
+                    },
+                  })}
                   className="mt-1 block w-full p-2 border border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500"
                 />
               </div>
+              {errors.message && (
+                <p className="text-sm text-red-700">
+                  {errors.message?.message}
+                </p>
+              )}
 
               <div>
                 <Button
@@ -74,7 +133,7 @@ const Contact = () => {
             <div className="space-y-6">
               {/* Image */}
               <Image
-                src={dummyImage}// Replace this with the actual image URL
+                src={dummyImage}
                 alt="Contact Us"
                 className="w-full h-auto object-cover rounded-md"
               />
---
+++ src/types/Types.ts
@@ -0,0 +1,5 @@
+export interface contactType {
+    name:string;
+    email:string;
+    message:string;
+}
