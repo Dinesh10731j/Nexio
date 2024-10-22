2d2f2d87cbf488c3cbf4a864afdab71bf545d7f4
Dinesh10731j <dinesh1122@gmail.com>
2024-10-22 19:47:34 +0545

    signup from validation completed

--- src/types/Types.ts
+++ src/types/Types.ts
@@ -2,4 +2,15 @@ export interface contactType {
     name:string;
     email:string;
     message:string;
+};
+
+
+
+export interface signupType{
+    name:string;
+    email:string;
+    password:string;
 }
+
+
+
